af3b6088cceb954d3178efa01cb4623cedb7f49f
luisj <luisj@LuisPc>
2024-05-14 12:11:25 +0200

    Actualizacion

---
+++ src/ProyectoEntornos/ClasePrincipal.java
@@ -0,0 +1,12 @@
+package ProyectoEntornos;
+
+public class ClasePrincipal {
+
+	public static void main(String[] args) {
+
+		System.out.println("Trabajo en grupo de entornos de darrollo, cada integrante pondra su nombre en una rama distinta");
+		System.out.println("Luis Portilla");
+
+	}
+
+}
